da88815f932e4a07d4e597095e7ac7bb5e2357c2
Alexander Moomaw <108152211+alhumaw@users.noreply.github.com>
2023-04-06 17:27:40 -0700

    Button Documentation
    wrote relevant comments on our button class to help with future modifications

--- src/Button.py
+++ src/Button.py
@@ -1,4 +1,8 @@
 class Button():
+    # The constructor sets the button's properties, such as its position and font. 
+    # It also creates a text object using the passed text and font, and stores it in the "text" property. 
+    # If no image is passed, the text object is used as the image of the button. 
+    # The "rect" and "text_rect" properties are set to create the rectangles around the button and the text, respectively.
     def __init__(self, image, pos, text_input, font, base_color, hovering_color):
         self.image = image
         self.x_pos = pos[0]
@@ -13,16 +17,19 @@ class Button():
         self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
 
+    # check for any requests(button hover)
     def update(self, window):
         if self.image is not None:
             window.blit(self.image, self.rect)
         window.blit(self.text, self.text_rect)
 
+    #check for any requests(button clicked)
+    #this code essentially waits for the mouse to hover over and click
     def checkForInput(self,position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
             return True
         return False
-    
+    #change the color if the button is hovered over
     def changeColor(self,position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
             self.text = self.font.render(self.text_input, True, self.hovering_color)
