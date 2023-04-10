ff4cc246949da8c2de548ff5fd82ea4e8c72d13b
RJ Belanger <70301845+RJBelanger@users.noreply.github.com>
2023-04-10 10:32:44 -0700

    game buttons added
    for second page next to add images above the buttons

--- src/main.py
+++ src/main.py
@@ -23,18 +23,47 @@ def play():
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
         #overwrite the screen with a black overlay
         window.fill("black")
-        PLAY_TEXT = get_font(45).render("This is the play screen", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640,260))
+        PLAY_TEXT = get_font(45).render("Pick A Game To Play", True, "Gold")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640,50))
         #render text to screen
         window.blit(PLAY_TEXT,PLAY_RECT)
         #clicking this will return back to the main menu
-        PLAY_BACK = Button(image=None, pos=(640,460),
-                            text_input="BACK", font=get_font(75), base_color="White", hovering_color = "Green")
+        Game1B = Button(image=None, pos=(250,350),
+                            text_input="Pong", font=get_font(50), base_color="White", hovering_color = "Green")
+    
+        Game2B = Button(image=None, pos=(620,350),
+                            text_input="Tic-Tac-Toe", font=get_font(50), base_color="White", hovering_color = "Green")
         
-        #change the color when the mouse is hovering
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(window)
+        Game3B = Button(image=None, pos=(990,350),
+                            text_input="DOOM", font=get_font(50), base_color="White", hovering_color = "Green")
 
+        Game4B = Button(image=None, pos=(250,650),
+                            text_input="Snake", font=get_font(50), base_color="White", hovering_color = "Green")
+    
+        Game5B = Button(image=None, pos=(620,650),
+                            text_input="Tower Defence", font=get_font(50), base_color="White", hovering_color = "Green")
+
+        Game6B = Button(image=None, pos=(990,650),
+                            text_input="Space Inv", font=get_font(50), base_color="White", hovering_color = "Green")
+
+        #change the color when the mouse is hovering
+        Game1B.changeColor(PLAY_MOUSE_POS)
+        Game1B.update(window)
+
+        Game2B.changeColor(PLAY_MOUSE_POS)
+        Game2B.update(window)
+
+        Game3B.changeColor(PLAY_MOUSE_POS)
+        Game3B.update(window)
+
+        Game4B.changeColor(PLAY_MOUSE_POS)
+        Game4B.update(window)
+
+        Game5B.changeColor(PLAY_MOUSE_POS)
+        Game5B.update(window)
+
+        Game6B.changeColor(PLAY_MOUSE_POS)
+        Game6B.update(window)
         for event in pygame.event.get():
             #if you press the "X" button, the game will exit
             if event.type == pygame.QUIT:
@@ -42,10 +71,11 @@ def play():
                 sys.exit()
             #if the mouse clicks the back button, return to main menu
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                if Game1B.checkForInput(PLAY_MOUSE_POS):
                     main_menu()
         #constantly update the screen. this is the critical piece of creating a game loo[]
         pygame.display.update()
+
 
 #options menu TBD
 def options():
@@ -75,6 +105,9 @@ def options():
                     main_menu()
 
         pygame.display.update()
+
+
+
 
 #Main menu implementation
 def main_menu():
